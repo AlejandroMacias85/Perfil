9e08e40065dee2cfacd6ee290f1cbc58b197d854
Luffy85 <macias85ale@gmail.com>
2022-10-12 18:04:15 +0000

    mi perfil

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,34 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./Navbar";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+	<body className="d-flex h-100 text-center text-bg-dark">
+    
+	<div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+	  <header className="mb-auto">
+		<Navbar />
+	  </header>
+	
+	  <main className="px-3">
+		<Jumbotron />
+	  </main>
+	
+	  <footer className="mt-auto text-white-50">
+		<p>Cover template for <a href="https://www.nesabamedia.com/wp-content/uploads/2018/10/background-img_02.png" className="text-black">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-black">@mdo</a>.</p>
+	  </footer>
+	</div>
+	
+	
+		
+	  
+	
+	</body>
+		</>
 	);
 };
 
